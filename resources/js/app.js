1b33a3bce8fcacafa978fc6003a66e0365f48e0a
tigernethost <mjserrano@tigernethost.com>
2024-09-30 15:43:55 +0800

    remove email adress in candidates

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,5 +1,4 @@
 import './bootstrap';
-
 import { createApp, h } from 'vue';
 import { createInertiaApp } from '@inertiajs/vue3';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
@@ -11,7 +10,6 @@ createInertiaApp({
     title: (title) => `${title} - ${appName}`,
     resolve: (name) => resolvePageComponent(`./Pages/${name}.vue`, import.meta.glob('./Pages/**/*.vue')),
     setup({ el, App, props, plugin }) {
-        // import('../css/app.css');
         return createApp({ render: () => h(App, props) })
             .use(plugin)
             .use(ZiggyVue)

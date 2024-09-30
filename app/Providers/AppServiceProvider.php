838868bd0633986aa8a9f6a8741bb004323f90aa
tigernethost <mjserrano@tigernethost.com>
2024-09-30 14:30:23 +0800

    add force https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use App\Models\Voter;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -20,6 +21,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        
+        if(env('FORCE_HTTPS')) {
+            URL::forceScheme('https');
+        }
     }
 }
